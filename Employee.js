1dfd5bad2c06409767ff37cc5bf0d872c02fb77a
Mona Lisa <cjensen@netechnical.com>
2023-01-04 13:50:26 -0600

    added questions in js for command line and added possilbe functions in employee.js

--- Employee.js
+++ Employee.js
@@ -19,6 +19,19 @@ getRole() {
     return Employee;
 }
 
-
-
 module.exports = Employee;
+
+// TODO: Create a function to generate HTML
+// function writeToFile(fileName, data) {
+//     return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+// }
+
+// TODO: Create a function to initialize app
+// function init() {
+//     inquirer.prompt(questions).then(inquireResponse => {
+//         writeToFile('README.md', markdown({...inquireResponse}))
+//     })
+// }
+
+// Function call to initialize app
+// init();
---
+++ script.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+const src = require('./src');
+
+const employeeInfo = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Team managers name',
+    }
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Employee ID',
+    }
+    {
+        type: 'input',
+        name: 'email',
+        message: 'email address',
+    }
+    {
+        type: 'input',
+        name: 'number',
+        message: 'Office number',
+    }
+    {
+        //option to choose this or to finnish building my team
+        type: 'list',
+        name: 'title',
+        choices: ['Engineer', 'Intern'],
+        //when engineer chosen; prompted to add engineer's name, ID, email, & github username; then taken back to the menu
+        //when intern chosen; prompted to enter intern's name, ID, email, and school; then taken back to the menu
+    }
+    //add finish building team to generate HTML
+]
