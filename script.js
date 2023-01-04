2fa10cf12f3a00aa9e536fa72da45eb248a79d15
Mona Lisa <cjensen@netechnical.com>
2023-01-04 14:01:27 -0600

    added jest test to package.json

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ const employeeInfo = [
         type: 'input',
         name: 'name',
         message: 'Team managers name',
-    }
+    },
     {
         type: 'input',
         name: 'id',
         message: 'Employee ID',
-    }
+    },
     {
         type: 'input',
         name: 'email',
         message: 'email address',
-    }
+    },
     {
         type: 'input',
         name: 'number',
         message: 'Office number',
-    }
+    },
     {
         //option to choose this or to finnish building my team
         type: 'list',
@@ -29,6 +29,6 @@ const employeeInfo = [
         choices: ['Engineer', 'Intern'],
         //when engineer chosen; prompted to add engineer's name, ID, email, & github username; then taken back to the menu
         //when intern chosen; prompted to enter intern's name, ID, email, and school; then taken back to the menu
-    }
+    },
     //add finish building team to generate HTML
 ]
