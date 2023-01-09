02daf627f36caf38774387a90ecebdd52c0f68e8
Mona Lisa <cjensen@netechnical.com>
2023-01-09 13:43:48 -0600

    edited the html template so it was pulling data correctly and edited the index.js to initiate the generateHtml function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const src = require('./src/html_template');
+const fs = require('fs');
+const path = require('path');
 
 const questions = [
     {
@@ -32,3 +34,18 @@ const questions = [
     },
     //add finish building team to generate HTML
 ]
+
+// TODO: Create a function to write HTML page
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(inquireResponse => {
+        writeToFile('html_template.html', src({...inquireResponse}))
+    })
+}
+
+// Function call to initialize app
+init();
